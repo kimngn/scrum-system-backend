84fdbd614e5225621615107b636c06e3b644394c
kixu <codewithkixu@gmail.com>
2026-07-23 00:26:29 -0500

    add more fields to userStory model, controller, get all userstories for backlog page

--- app/models/userStory.model.js
+++ app/models/userStory.model.js
@@ -1,5 +1,10 @@
 module.exports = (sequelize, Sequelize) => {
   const UserStory = sequelize.define("userStory", {
+    story_id: {
+      type: Sequelize.INTEGER,
+      primaryKey: true,
+      autoIncrement: true
+    },
     title: {
       type: Sequelize.STRING,
       allowNull: false,
@@ -16,6 +21,50 @@ module.exports = (sequelize, Sequelize) => {
       type: Sequelize.INTEGER,
       allowNull: true,
     },
+      status: {
+      type: Sequelize.STRING(255)
+    },
+
+    type: {
+      type: Sequelize.STRING(255)
+    },
+
+    createdDate: {
+      type: Sequelize.DATE,
+      defaultValue: Sequelize.NOW
+    },
+
+    updatedDate: {
+      type: Sequelize.DATE
+    },
+
+    dueDate: {
+      type: Sequelize.DATE
+    },
+
+    completedDate: {
+      type: Sequelize.DATE
+    },
+
+    Sprint_id: {
+      type: Sequelize.INTEGER
+    },
+
+     Project_id: {
+      type: Sequelize.INTEGER
+    },
+
+    assignedUser_id: {
+      type: Sequelize.INTEGER
+    },
+
+    createdBy_id: {
+      type: Sequelize.INTEGER
+    },
+
+    updatedBy_id: {
+      type: Sequelize.INTEGER
+    }
   });
   return UserStory;
 };
