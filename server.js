a19aecfbe77d4b8c5c677b19ec1c450cb0b89fc9
Magnani Fabiola <magnanfabiola@gmail.com>
2026-08-04 11:24:32 -0500

    remove team/teamMember scope, use project-only membership

--- server.js
+++ server.js
@@ -50,7 +50,6 @@ require("./app/routes/github.routes")(app); // passes app to Github route file
 require("./app/routes/repo.routes.js")(app);
 require("./app/routes/history.routes.js")(app);
 require("./app/routes/projectMembership.routes.js")(app);
-require("./app/routes/team.routes.js")(app);
 require("./app/routes/storyAssignee.routes.js")(app);
 // set port, listen for requests
 const PORT = process.env.PORT || 3200;
