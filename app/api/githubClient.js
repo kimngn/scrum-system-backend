b9cc8d28af8ef630441a1151cfa891ff945c4286
Ashley <ashphamley@gmail.com>
2026-08-10 11:49:06 -0500

    PostPullRequest

--- app/api/githubClient.js
+++ app/api/githubClient.js
@@ -44,14 +44,49 @@ const getBranches = async () => {
   }
 };
 
-const getPullRequests = async () => {
+const getPullRequests = async (token, owner, repoName, branchName) => {
+  console.log("OWNER:" + owner);
+  console.log("branchName:" + branchName);
+  const api = useGithubClient(token); // prepare the API to make calls to Github
   try {
-    const response = await api.get("/repos/kimngn/scrum-system-backend/pulls");
-    return response.data;
+    const response = await api.get(
+      `/repos/${owner}/${repoName}/pulls?state=all&per_page=100`, // fetch all PRs (can't get branchName to match exact branch format)
+      // funny bug where first 30 PRs are fetched because of 30 PRs per page
+    );
+
+    console.log("All PRs:");
+
+    for (const pr of response.data) {
+      console.log("PR head.ref:", pr.head.ref);
+      console.log("PR branchname:", branchName);
+    }
+
+    const filtered = response.data.filter((pr) => pr.head.ref === branchName); // filter fetched PRs by branchName
+
+    return filtered.map((pr) => ({
+      // map PR to an object (remove data that won't be used)
+      author: pr.user.login,
+      number: pr.number,
+      title: pr.title,
+      state: pr.state,
+      createdAt: pr.created_at,
+      mergedAt: pr.merged_at,
+      mergedBy: pr.merged_by?.login ?? null,
+      assignee: pr.assignee?.login ?? null,
+      url: pr.html_url,
+    }));
   } catch (error) {
     console.error(error.response?.data || error.message);
     throw error;
   }
+};
+
+const postPullRequest = async (story, pr) => {
+  // need title, body, head, base
+
+  console.log("Story Title:" + story.title);
+  console.log("Story Description:" + story.body);
+  const api = useGithubClient(token); // prepare the API to make calls to Github
 };
 
 module.exports = {
@@ -59,4 +94,5 @@ module.exports = {
   getBranches,
   getPullRequests,
   getRepoData,
+  postPullRequest,
 };
